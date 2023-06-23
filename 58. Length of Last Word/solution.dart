c7cbbb29120ea06160582b948236e441bd37d27a
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2023-06-23 15:19:08 +0200

    58

---
+++ 58. Length of Last Word/solution.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int lengthOfLastWord(String s) {
+    int count = 0;
+    for(int i = s.length - 1; i >= 0; i--){
+        if(s[i] == " "){
+          if(count != 0) break;
+        } else  {
+          count++;
+        }
+    }
+    return count;
+  }
+}
