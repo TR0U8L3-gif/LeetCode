07264ebc1fc15f95bf4dd92e116fd10fd6d547a7
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2023-07-01 11:58:24 +0200

    2305

---
+++ 2305. Fair Distribution of Cookies/solution.dart
@@ -0,0 +1,24 @@
+class Solution {
+    int distributeCookies(List<int> cookies, int k) {
+        if(cookies.isEmpty) return 0;     
+        return dfs(0, List<int>.filled(k,0), cookies, k);
+    }
+
+    int dfs(int i, List<int> children, List<int> cookies, int k){
+        if(i == cookies.length){
+            int unfairness = double.minPositive.toInt();
+            for(int value in children){
+                unfairness = max(unfairness, value);
+            }
+            return unfairness;
+        }
+
+        int answer = double.maxFinite.toInt();
+        for (int j = 0; j < k; ++j) {
+            children[j] += cookies[i];
+            answer = min(answer, dfs(i + 1, children, cookies, k));
+            children[j] -= cookies[i];
+        }
+        return answer;
+    }
+}
